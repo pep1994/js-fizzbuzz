1baf9d5fe095ad3bceb199ef9b954b3690e0545e
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-26 15:08:16 +0100

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-//* Fizz Buzz test:
+/*
+Fizz Buzz test:
+
 //  Scrivi un programma che stampi i numeri da 1 a 100,
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi “Buzz”.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi ”FizzBuzz”. *//
+// Per i numeri che sono sia multipli di 3 che di 5 stampi ”FizzBuzz”.
+
+*/
 
 
 
